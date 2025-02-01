c4dd00dfff417b9d7d54884a91d2694221fb457d
Roman <isyk.roman@gmail.com>
2025-02-01 17:11:39 +0200

    added plugin ace

---
+++ js/global.js
@@ -0,0 +1,30 @@
+document.addEventListener('DOMContentLoaded', () => {
+    (function() {
+
+    	"use strict"
+
+    	const $BlockEditors = document.querySelectorAll('.block-code-editor')
+
+		if ($BlockEditors?.length) {
+		    $BlockEditors.forEach(($e) => {
+		    	const dm = $e.getAttribute('data-mode')
+		    	let m = dm ? dm : 'javascript'
+
+		        ace.edit($e, {
+		            theme: "ace/theme/monokai",
+		            mode: "ace/mode/" + m,
+		            fontSize: 16,
+		            selectionStyle: "text",
+		            maxLines: 30,
+		            minLines: 2,
+		        })     
+
+		    })
+		}
+
+		
+       
+    })();
+})
+
+        
